0d157b0c648a6358f1dfc91a48b51dbdf90e1e75
milkcya <milkcya444@gmail.com>
2025-05-22 17:40:22 +0300

    Create score = 0.py

---
+++ score = 0.py
@@ -0,0 +1,36 @@
+score = 0
+
+
+def has_digit(password):
+    return any(char.isdigit() for char in password)
+
+
+def is_very_long(password):
+    return len(password) > 12
+
+
+def has_lower_letters(password):
+    return any(char.islower() for char in password)
+
+
+def has_upper_letters(password):
+    return any(char.isupper() for char in password)
+
+
+def has_symbols(password):
+    return any(not char.isalnum() for char in password)
+
+
+password = input("Введите пароль: ")
+
+checks = [
+    has_digit,
+    is_very_long,
+    has_lower_letters,
+    has_upper_letters,
+    has_symbols
+]
+    
+score = sum(2 for check in checks if check(password))
+
+print(f"Рейтинг пароля: {score}")
